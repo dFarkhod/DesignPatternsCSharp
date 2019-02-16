987c29bd24d9588c68e6a9e33588d0fe8958d1b7
Farkhod Dadajanov <dfarkhod@gmail.com>
2019-02-16 15:08:20 +0800

    added singleton design pattern sample project

---
+++ SingletoneDesignPattern/AppConfigManager.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Configuration;
+using System.Text;
+using System.Linq;
+
+namespace SingletoneDesignPattern
+{
+    public sealed class AppConfigManager
+    {
+        private static readonly Lazy<AppConfigManager> lazy = new Lazy<AppConfigManager>(() => new AppConfigManager());
+        private AppConfigManager()
+        {
+            Initialize();
+        }
+
+        public static AppConfigManager Instance => lazy.Value;
+
+        public Dictionary<string, string> AppSettings { get; private set; }
+        public Dictionary<string, string> ConnectionStrings { get; private set; }
+
+        public T GetAppSetting<T>(string key) where T : IConvertible
+        {
+            T result = default(T);
+            try
+            {
+                var value = AppSettings[key];
+                result = (T)Convert.ChangeType(value, typeof(T));
+            }
+            catch
+            {
+                //Could not convert.  Pass back default value...
+                result = default(T);
+            }
+
+            return result;
+        }
+
+
+        public void Initialize()
+        {
+            if (!ConfigurationManager.AppSettings.HasKeys())
+            {
+                Console.WriteLine("No appsettings secion found in the config file.");
+            }
+
+            AppSettings = new Dictionary<string, string>();
+            ConnectionStrings = new Dictionary<string, string>();
+
+            foreach (string key in ConfigurationManager.AppSettings.AllKeys)
+            {
+                string value = ConfigurationManager.AppSettings[key];
+                AppSettings.Add(key, value);
+            }
+
+            foreach (ConnectionStringSettings item in ConfigurationManager.ConnectionStrings)
+            {
+                ConnectionStrings.Add(item.Name, item.ConnectionString);
+            }
+        }
+    }
+}
---
+++ SingletoneDesignPattern/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Configuration;
+using System.Diagnostics;
+
+namespace SingletoneDesignPattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stopwatch watch = Stopwatch.StartNew();
+            // get value as string:
+            string stringValFromConfig = AppConfigManager.Instance.AppSettings["integerSetting"];
+            watch.Stop();
+            long elapsedMs = watch.ElapsedMilliseconds;
+
+            Console.WriteLine(stringValFromConfig);
+            Console.WriteLine($"Elapsed time: {elapsedMs} ms.");
+
+
+            watch = Stopwatch.StartNew();
+            // get value as any other type:
+            bool boolValFromConfigFromManager = AppConfigManager.Instance.GetAppSetting<bool>("boolSetting");
+            watch.Stop();
+            elapsedMs = watch.ElapsedMilliseconds;
+
+            Console.WriteLine(boolValFromConfigFromManager);
+            Console.WriteLine($"Elapsed time: {elapsedMs} ms.");
+
+
+            watch = Stopwatch.StartNew();
+            string stringValueFromConfigFile = ConfigurationManager.AppSettings["stringSetting"];
+            Console.WriteLine(stringValueFromConfigFile);
+            Console.WriteLine($"Elapsed time: {elapsedMs} ms.");
+
+            Console.ReadLine();
+
+            watch = Stopwatch.StartNew();
+            stringValueFromConfigFile = ConfigurationManager.AppSettings["stringSetting"];
+            Console.WriteLine(stringValueFromConfigFile);
+            Console.WriteLine($"Elapsed time: {elapsedMs} ms.");
+
+            Console.ReadLine();
+        }
+    }
+}
